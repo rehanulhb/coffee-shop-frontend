b2beabd5ec011f206c6f67355c7b3a28b0313cf6
rehanulhb <rehanulh@gmail.com>
2024-12-27 22:45:50 +0100

    Delete function done for Frontend with Sweet Alert

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,5 +1,45 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire({
+        //   title: "Deleted!",
+        //   text: "Your file has been deleted.",
+        //   icon: "success"
+        // });
+
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method : 'DELETE'
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure>
@@ -16,7 +56,12 @@ const CoffeeCard = ({ coffee }) => {
           <div className="join join-vertical space-y-4">
             <button className="btn">View</button>
             <button className="btn">Edit</button>
-            <button className="btn">X</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-red-500"
+            >
+              X
+            </button>
           </div>
         </div>
       </div>
