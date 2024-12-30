3a68029492ca90261d180861a7d2de53d4f5f3a9
rehanulhb <rehanulh@gmail.com>
2024-12-30 19:55:29 +0100

    Sign Up Page Design done, and receiving its form value

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,57 @@
-
-
 const SignUp = () => {
-    return (
-        <div>
-            <h2>Please Sign up</h2>
+    
+    const handleSignUp = e =>{
+        e.preventDefault();
+        const form = e.target;
+        console.log(form.email.value, form.password.value)
+    }
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Sign up now!</h1>
+          
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleSignUp} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign up</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
